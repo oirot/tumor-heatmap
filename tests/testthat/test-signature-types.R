0469808c63fd031ebf6cffc8a64e1da226ac9820
Vittorio Aiello <vittorio.aiello@mail.polimi.it>
2021-06-07 05:07:21 +0200

    Added actual unit testing

---
+++ tests/testthat/test-signature-types.R
@@ -0,0 +1,14 @@
+test_that("Signature type conversion", {
+  expect_equal(getGenomeType(signatureTypes$alexandrov2), "Alexandrov")
+  expect_equal(getGenomeType(signatureTypes$alexandrov32), "Alexandrov")
+  expect_equal(getGenomeType(signatureTypes$shiraishi), "Shiraishi")  
+})
+
+test_that("Returning correct signature reading function", {
+  expect_equal(getReadSignatureFunction(signatureTypes$alexandrov2), 
+               decompTumor2Sig::readAlexandrovSignatures)
+  expect_equal(getReadSignatureFunction(signatureTypes$shiraishi), 
+               decompTumor2Sig::readShiraishiSignatures)
+  expect_equal(getReadSignatureFunction(signatureTypes$alexandrov32), 
+               readAlexandrovV32Signatures)
+})
