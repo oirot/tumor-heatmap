d7ea1ddc54be3ef42040e8afbf7c2bb17496a340
Vittorio Aiello <vittorio.aiello@mail.polimi.it>
2021-06-28 00:42:52 +0200

    Added data documentation

---
+++ inst/script/COSMIC_v3.2_SBS_GRCh37.R
@@ -0,0 +1,95 @@
+#' COSMIC SBS v3.2 GRCh37
+#'
+#' A csv file containing the COSMIC v3.2 SBS signatures based on the human
+#' GRCh37 assembly. The variables are as follows:
+#'
+#' \itemize{
+#'  \item Type. the type of signle base substitution and its context (A[C>A]A)
+#'  \item SBS1. the frequencies of substitution for SBS1 signature
+#'	\item SBS2. the frequencies of substitution for the signature
+#'	\item SBS3. the frequencies of substitution for the signature
+#'	\item SBS4. the frequencies of substitution for the signature
+#'	\item SBS5. the frequencies of substitution for the signature
+#'	\item SBS6. the frequencies of substitution for the signature
+#'	\item SBS7a. the frequencies of substitution for the signature
+#'	\item SBS7b. the frequencies of substitution for the signature
+#'	\item SBS7c. the frequencies of substitution for the signature
+#'	\item SBS7d. the frequencies of substitution for the signature
+#'	\item SBS8. the frequencies of substitution for the signature
+#'	\item SBS9. the frequencies of substitution for the signature
+#'	\item SBS10a. the frequencies of substitution for the signature
+#'	\item SBS10b. the frequencies of substitution for the signature
+#'	\item SBS10c. the frequencies of substitution for the signature
+#'	\item SBS10d. the frequencies of substitution for the signature
+#'	\item SBS11. the frequencies of substitution for the signature
+#'	\item SBS12. the frequencies of substitution for the signature
+#'	\item SBS13. the frequencies of substitution for the signature
+#'	\item SBS14. the frequencies of substitution for the signature
+#'	\item SBS15. the frequencies of substitution for the signature
+#'	\item SBS16. the frequencies of substitution for the signature
+#'	\item SBS17a. the frequencies of substitution for the signature
+#'	\item SBS17b. the frequencies of substitution for the signature
+#'	\item SBS18. the frequencies of substitution for the signature
+#'	\item SBS19. the frequencies of substitution for the signature
+#'	\item SBS20. the frequencies of substitution for the signature
+#'	\item SBS21. the frequencies of substitution for the signature
+#'	\item SBS22. the frequencies of substitution for the signature
+#'	\item SBS23. the frequencies of substitution for the signature
+#'	\item SBS24. the frequencies of substitution for the signature
+#'	\item SBS25. the frequencies of substitution for the signature
+#'	\item SBS26. the frequencies of substitution for the signature
+#'	\item SBS27. the frequencies of substitution for the signature
+#'	\item SBS28. the frequencies of substitution for the signature
+#'	\item SBS29. the frequencies of substitution for the signature
+#'	\item SBS30. the frequencies of substitution for the signature
+#'	\item SBS31. the frequencies of substitution for the signature
+#'	\item SBS32. the frequencies of substitution for the signature
+#'	\item SBS33. the frequencies of substitution for the signature
+#'	\item SBS34. the frequencies of substitution for the signature
+#'	\item SBS35. the frequencies of substitution for the signature
+#'	\item SBS36. the frequencies of substitution for the signature
+#'	\item SBS37. the frequencies of substitution for the signature
+#'	\item SBS38. the frequencies of substitution for the signature
+#'	\item SBS39. the frequencies of substitution for the signature
+#'	\item SBS40. the frequencies of substitution for the signature
+#'	\item SBS41. the frequencies of substitution for the signature
+#'	\item SBS42. the frequencies of substitution for the signature
+#'	\item SBS43. the frequencies of substitution for the signature
+#'	\item SBS44. the frequencies of substitution for the signature
+#'	\item SBS45. the frequencies of substitution for the signature
+#'	\item SBS46. the frequencies of substitution for the signature
+#'	\item SBS47. the frequencies of substitution for the signature
+#'	\item SBS48. the frequencies of substitution for the signature
+#'	\item SBS49. the frequencies of substitution for the signature
+#'	\item SBS50. the frequencies of substitution for the signature
+#'	\item SBS51. the frequencies of substitution for the signature
+#'	\item SBS52. the frequencies of substitution for the signature
+#'	\item SBS53. the frequencies of substitution for the signature
+#'	\item SBS54. the frequencies of substitution for the signature
+#'	\item SBS55. the frequencies of substitution for the signature
+#'	\item SBS56. the frequencies of substitution for the signature
+#'	\item SBS57. the frequencies of substitution for the signature
+#'	\item SBS58. the frequencies of substitution for the signature
+#'	\item SBS59. the frequencies of substitution for the signature
+#'	\item SBS60. the frequencies of substitution for the signature
+#'	\item SBS84. the frequencies of substitution for the signature
+#'	\item SBS85. the frequencies of substitution for the signature
+#'	\item SBS86. the frequencies of substitution for the signature
+#'	\item SBS87. the frequencies of substitution for the signature
+#'	\item SBS88. the frequencies of substitution for the signature
+#'	\item SBS89. the frequencies of substitution for the signature
+#'	\item SBS90. the frequencies of substitution for the signature
+#'	\item SBS91. the frequencies of substitution for the signature
+#'	\item SBS92. the frequencies of substitution for the signature
+#'	\item SBS93. the frequencies of substitution for the signature
+#'	\item SBS94. the frequencies of substitution for the signature 
+#' }
+#' 
+#' @format A csv file with 96 rows (one for each possible SBS and context) 
+#' and 79 columns
+#' @docType data
+#' @keywords datasets csv signatures COSMIC SBS
+#' @name COSMIC_v3.2_SBS_GRCh37
+#' @usage COSMIC_v3.2_SBS_GRCh37
+#' @source \url{https://cancer.sanger.ac.uk/signatures/downloads/}
+"COSMIC_v3.2_SBS_GRCh37"
